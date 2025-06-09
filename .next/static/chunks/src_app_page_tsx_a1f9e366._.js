f38e8790d5b9c3d0fc53015890724e819e3d6f43
trung-sys <trungcaidum2@gmail.com>
2025-06-10 03:38:30 +0700

    Add .next/static/chunks/src_app_page_tsx_a1f9e366._.js 1749501433951

---
+++ .next/static/chunks/src_app_page_tsx_a1f9e366._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_07ffe254._.js",
+  "static/chunks/src_497afb50._.js",
+  "static/chunks/src_app_animations_4dea4eb4.css"
+],
+    source: "dynamic"
+});
